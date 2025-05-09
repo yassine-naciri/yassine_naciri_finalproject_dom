3c54e3ed8074e7f41f16185f90c13e9f8c2a962d
Yassine Naciri <naciri1y@gmail.com>
2025-05-09 16:47:51 +0100

    booking btn

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+let btn = document.getElementById("booking_btn")
+
+
+
+
+
+btn.addEventListener("click",function(){
+    console.log(btn)
+    let name = prompt("Enter Name")
+    let email = prompt("Enter Your Email")
+    alert(name +" You will receive an email for More informations")
+
+    
+    
+})
+
+
